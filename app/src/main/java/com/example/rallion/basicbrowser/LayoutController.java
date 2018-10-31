54c8d6eb55f1cc0d21f1a5592ba3d468c9691d06
guy.sartorelli@gmail.com <guy.sartorelli@gmail.com>
2018-11-01 11:59:18 +1300

    allowed javascript and cookies

--- app/src/main/java/com/example/rallion/basicbrowser/LayoutController.java
+++ app/src/main/java/com/example/rallion/basicbrowser/LayoutController.java
@@ -1,11 +1,11 @@
 package com.example.rallion.basicbrowser;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
+import android.webkit.CookieManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.EditText;
 
 class LayoutController {
@@ -23,6 +23,13 @@ class LayoutController {
 
         WebView webView = activity.findViewById(R.id.web_view);
         webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        } else {
+            CookieManager.getInstance().setAcceptCookie(true);
+        }
+
         EditText locationInput = activity.findViewById(R.id.location_input);
         locationInput.setOnEditorActionListener((view, actionId, event) -> {
             webView.loadUrl(URLParser.parse(locationInput.getText().toString()));
